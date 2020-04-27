7de8c202df3818bf1205e825899c2cdca897824a
culberbn <culberbn@mail.uc.edu>
2020-04-27 12:34:03 -0400

    gpio changes

--- api/test.py
+++ api/test.py
@@ -4,6 +4,7 @@ import RPi.GPIO as GPIO
 
 app = Flask(__name__)
 CORS(app)
+GPIO.setmode(GPIO.BOARD)
 
 @app.route('/api/getstatus', methods=['GET'])
 def get_status():
@@ -11,12 +12,12 @@ def get_status():
 
 if __name__ == '__main__':
     GPIO.setup(12, GPIO.OUT) # red
-    GPIO.setup(13, GPIO.OUT) # green
-    GPIO.setup(19, GPIO.OUT) # blue
+    # GPIO.setup(13, GPIO.OUT) # green
+    # GPIO.setup(19, GPIO.OUT) # blue
     red = GPIO.PWM(12, 1000)
-    green = GPIO.PWM(13, 1000)
-    blue = GPIO.PWM(19, 1000)
+    # green = GPIO.PWM(13, 1000)
+    # blue = GPIO.PWM(19, 1000)
     red.start(50)
-    blue.start(50)
-    green.start(50)
+    # blue.start(50)
+    # green.start(50)
     app.run(debug=True)
