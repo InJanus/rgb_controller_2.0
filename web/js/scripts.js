a27dc860e2f27b21075bb44d15996af28d043e94
culberbn <culberbn@mail.uc.edu>
2020-04-27 12:25:45 -0400

    init

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -1,8 +1,6 @@
-var myApp = angular.module('myApp', []);
-
-myApp.controller('textCtrl', function textCtrl($scope, $http){
-    $http.get('http://127.0.0.1:5000/api/getstatus').then(function(data){
-        $scope.photos = data.data;
+var appname = angular.module('appname', []);
+appname.controller('appCtrl', ['$scope', '$http', function($scope, $http) {
+    $http.get("http://127.0.0.1:5000/api/getstatus").then(function(data){
+        $scope.status = data.data;
     });
-});
-    
+}]);
